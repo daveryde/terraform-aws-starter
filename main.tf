e6e5f0b9792a6fa56c09ab802ac7ffaa8d3c25d7
David Cordova <d.mcordova87@gmail.com>
2021-10-27 11:36:58 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,154 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAXMWD35OMI4MUYIMO"
+  secret_key = "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"
+}
+
+# 1. Create VPC
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+# 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# 4. Create a subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# 5. Associate subnet with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow port 22, 80, 443
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description     = "HTTPS"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self            = false
+  }
+
+  ingress {
+    description     = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self            = false
+  }
+
+  ingress {
+    description     = "SSH"
+    from_port       = 20
+    to_port         = 20
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self            = false
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+# 9. Create Amazon Linux 2 AMI server and install/enable apache2
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-074cce78125f09d61"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name          = "main-key"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update
+                sudo yum install -y httpd
+                sudo chkconfig httpd on
+                sudo service httpd start
+                echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+                EOF
+  tags = {
+    Name = "web-server"
+  }
+}
